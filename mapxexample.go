2499a76b767da0f4df182720b65a5856c229a54f
gs-akhan <akhan@gainsight.com>
2015-03-08 16:44:31 +0530

    Fixing map

--- mapxexample.go
+++ mapxexample.go
@@ -3,17 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	var salutationMap map[string]string
 	
+	var salutationMap map[string]string
 	salutationMap = make(map[string]string)
 
-	salutationMap['oldname'] = "azharkhan"
-
+	salutationMap["oldname"] = "azharuddin khan"
 	myMap := map[string]string {
 		"name" : "azhar",
 	}
 
 	myMap["age"] = "12"
+
 	myMap["location"] = "Hyderbad"
 
 	fmt.Println(myMap["location"])
